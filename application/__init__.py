be9a11639c94575ce776c15454cbff1234e5ae5a
otsha <32258613+otsha@users.noreply.github.com>
2018-03-23 22:05:27 +0200

    Updates to requirements and database model

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_ECHO"] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create db
 db = SQLAlchemy(app)
--- application/database/models.py
+++ application/database/models.py
@@ -3,8 +3,7 @@ from application import db
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default = db.func.current_timestamp())
-    onupdate = db.func.current_timestamp()
+    date_modified = db.Column(db.DateTime, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
 
     content = db.Column(db.String(140), nullable = False)
     read = db.Column(db.Boolean, nullable = False)
