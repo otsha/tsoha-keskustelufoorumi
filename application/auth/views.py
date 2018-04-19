1a46f5f53ee512927877e172fff05749bab8fdb4
otsha <32258613+otsha@users.noreply.github.com>
2018-04-19 13:35:19 +0300

    Add functionality for viewing users and changing their super status

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,46 +1,51 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 from application import app, db
 from application.auth.models import User
 from application.auth.forms import LoginForm, RegisterForm
 
 # Handle the login page
-@app.route("/auth/login", methods = ["GET", "POST"])
+
+
+@app.route("/auth/login", methods=["GET", "POST"])
 def auth_login():
     if request.method == "GET":
-        return render_template("auth/loginform.html", form = LoginForm())
+        return render_template("auth/loginform.html", form=LoginForm())
 
     form = LoginForm(request.form)
 
-    user = User.query.filter_by(username = form.username.data, password = form.password.data).first()
+    user = User.query.filter_by(
+        username=form.username.data, password=form.password.data).first()
     if not user:
-        return render_template("auth/loginform.html", form = form, error = "Incorrect username or password")
-    
+        return render_template("auth/loginform.html", form=form, error="Incorrect username or password")
+
     # Return to the dashboard
     login_user(user)
     print("User " + user.username + " authenticated")
     return redirect(url_for("messages_index"))
 
 # Handle the register page
-@app.route("/auth/register", methods = ["GET", "POST"])
+
+
+@app.route("/auth/register", methods=["GET", "POST"])
 def auth_register():
     if request.method == "GET":
-        return render_template("auth/registerform.html", form = RegisterForm())
-    
+        return render_template("auth/registerform.html", form=RegisterForm())
+
     form = RegisterForm(request.form)
 
     if not form.validate():
-        return render_template("auth/registerform.html", form = form)
+        return render_template("auth/registerform.html", form=form)
 
     # Check if username already in use
-    user = User.query.filter_by(username = form.username.data).first()
+    user = User.query.filter_by(username=form.username.data).first()
     if user:
-        return render_template("auth/registerform.html", form = form, error = "Username already in use")
+        return render_template("auth/registerform.html", form=form, error="Username already in use")
 
     # Add new account to database
     a = User(form.username.data, form.password.data)
-    
+
     db.session().add(a)
     db.session().commit()
 
@@ -48,7 +53,53 @@ def auth_register():
     return redirect(url_for("auth_login"))
 
 # Handle logging out
+
+
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
     return redirect(url_for("index"))
+
+# GET User page
+
+
+@app.route("/users/<user_id>", methods=["GET"])
+def view_user(user_id):
+    u = User.query.get(user_id)
+    return render_template("auth/user.html", user=u)
+
+# POST Promote user to admin
+
+
+@app.route("/users/<user_id>/promote", methods=["POST"])
+@login_required
+def promote_user(user_id):
+    u = User.query.get(user_id)
+
+    # Make sure that the current user is allowed to do this
+    if not current.user.isSuper:
+        return redirect(url_for("view_user", user_id=u.id))
+
+    u.isSuper = True
+    db.session().add(u)
+    db.session().commit()
+
+    return redirect(url_for("view_user", user_id=u.id))
+
+
+# POST Demote user from admin
+@app.route("/users/<user_id>/demote", methods=["POST"])
+@login_required
+def demote_user(user_id):
+    u = User.query.get(user_id)
+
+    u.isSuper = False
+    db.session().add(u)
+    db.session().commit()
+
+    # Make sure that the current user is allowed to do this
+
+    if not current.user.isSuper:
+        return redirect(url_for("view_user", user_id=u.id))
+
+    return redirect(url_for("view_user", user_id=u.id))
